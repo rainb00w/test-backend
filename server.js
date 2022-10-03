fe4abbb95383d0bd86be29ce9dff7035dcd7f3b2
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-10-03 06:00:14 +0300

    _v2g

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const app = require('./app');
 const mongoose = require("mongoose");
 const {DB_HOST, PORT = 3000} = process.env;
 
+
+
 mongoose.connect(DB_HOST)
-.then(()=> {
-  app.listen(process.env.PORT || 3001, () => {
-    console.log(`Database connection successful on port ${PORT}`);
-
-})
-.catch(error => {
-  process.exit(1);
-});
-
+    .then(() => app.listen(process.env.PORT || 3001, () => {
+        console.log(`Database connection successful on port ${PORT}`)
+    })
+    )
+    .catch(err => {
+        console.error(err)
+        process.exit(1)
+    });
